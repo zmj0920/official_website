acc529bf985efc172ef388b3fe27cf66b4cd3cb5
mengjie.zhang <mengjie.zhang@easystack.cn>
2021-11-24 10:10:34 +0800

    First commit

---
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -0,0 +1,46 @@
+module.exports = [
+  {
+    text: "网站首页",
+    link: "/",
+  },
+  {
+    text: "成功案例",
+    link: "/web/",
+    items: [],
+  },
+  {
+    text: "产品展示",
+    link: "/admin/",
+    items: [],
+  },
+  {
+    text: "设备展示",
+    link: "/technology/",
+    items: [],
+  },
+  {
+    text: "行业资讯",
+    link: "/cloudcomputing/",
+    items: [],
+  },
+  {
+    text: "关于我们",
+    link: "/pages/beb6c0bd8a66cea6/",
+    items: [],
+  },
+  {
+    text: "姿势荣誉",
+    link: "/pages/e14099/",
+    items: [],
+  },
+  {
+    text: "联系我们",
+    link: "/pages/ad247c4332211551/",
+    items: [],
+  },
+  {
+    text: "Article timeline",
+    link: "/blog/",
+    items: [],
+  },
+];
---
+++ docs/index.md
@@ -0,0 +1,4 @@
+---
+home: true
+bannerBg: http://www.czfxwy.com/Upload/atm/5BB9BEABBFE239293D95E1ABB7FF9570.jpg
+---
